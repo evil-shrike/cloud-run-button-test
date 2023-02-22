6babfe3aa86016254cb94fe05f945950fda65561
Sergei Dorogin <segy@google.com>
2023-02-22 18:47:55 +0400

    test

--- postcreate.sh
+++ postcreate.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
-
 set -e
 set -x
 
-echo "Setting Project ID: $GOOGLE_CLOUD_PROJECT"
-gcloud config set project $GOOGLE_CLOUD_PROJECT
-
-gcloud services enable cloudresourcemanager.googleapis.com
-
-PROJECT_NUMBER=$(gcloud projects describe $GOOGLE_CLOUD_PROJECT --format="csv(projectNumber)" | tail -n 1)
-SERVICE_ACCOUNT=$PROJECT_NUMBER-compute@developer.gserviceaccount.com
-
-gcloud projects add-iam-policy-binding $GOOGLE_CLOUD_PROJECT --member=serviceAccount:$SERVICE_ACCOUNT --role=roles/resourcemanager.projectIamAdmin
+#echo "Setting Project ID: $GOOGLE_CLOUD_PROJECT"
+#gcloud config set project $GOOGLE_CLOUD_PROJECT
+#gcloud services enable cloudresourcemanager.googleapis.com
 
 gcloud run services update $K_SERVICE --execution-environment=gen2 --timeout=3600 --max-instances=1 --region=$GOOGLE_CLOUD_REGION
--- prebuild.sh
+++ prebuild.sh
@@ -1,4 +1,24 @@
-echo "K_SERVICE: $K_SERVICE"
-echo "GOOGLE_CLOUD_REGION: $GOOGLE_CLOUD_REGION"
-echo "GOOGLE_CLOUD_PROJECT: $GOOGLE_CLOUD_PROJECT"
-echo "APP_DIR: $APP_DIR"
+#!/bin/bash
+set -e
+set -x
+
+#echo "K_SERVICE: $K_SERVICE"
+#echo "GOOGLE_CLOUD_REGION: $GOOGLE_CLOUD_REGION"
+#echo "GOOGLE_CLOUD_PROJECT: $GOOGLE_CLOUD_PROJECT"
+#echo "APP_DIR: $APP_DIR"
+
+gcloud config set project $GOOGLE_CLOUD_PROJECT
+
+PROJECT_NUMBER=$(gcloud projects describe $GOOGLE_CLOUD_PROJECT --format="csv(projectNumber)" | tail -n 1)
+SERVICE_ACCOUNT=$PROJECT_NUMBER-compute@developer.gserviceaccount.com
+
+gcloud projects add-iam-policy-binding $GOOGLE_CLOUD_PROJECT --member=serviceAccount:$SERVICE_ACCOUNT --role=roles/resourcemanager.projectIamAdmin
+
+gcloud services enable cloudresourcemanager.googleapis.com
+gcloud services enable artifactregistry.googleapis.com
+gcloud services enable run.googleapis.com
+gcloud services enable iamcredentials.googleapis.com
+gcloud services enable cloudbuild.googleapis.com
+gcloud services enable bigquery.googleapis.com
+gcloud services enable cloudfunctions.googleapis.com
+gcloud services enable googleads.googleapis.com
