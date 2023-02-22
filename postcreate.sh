84e0b0ee3d884c7fd85b8231e0db28b1817bae27
Sergei Dorogin <segy@google.com>
2023-02-22 16:15:33 +0400

    test

--- postcreate.sh
+++ postcreate.sh
@@ -1,2 +1,11 @@
 # TODO: how can we get the current GCP project id here?
-#gcloud services enable cloudresourcemanager.googleapis.com
+
+echo "Setting Project ID: ${GOOGLE_CLOUD_PROJECT}"
+gcloud config set project ${GOOGLE_CLOUD_PROJECT}
+
+gcloud services enable cloudresourcemanager.googleapis.com
+
+PROJECT_NUMBER=$(gcloud projects describe $GOOGLE_CLOUD_PROJECT --format="csv(projectNumber)" | tail -n 1)
+SERVICE_ACCOUNT=$PROJECT_NUMBER-compute@developer.gserviceaccount.com
+
+gcloud projects add-iam-policy-binding $GOOGLE_CLOUD_PROJECT --member=serviceAccount:$SERVICE_ACCOUNT --role=roles/resourcemanager.projectIamAdmin
