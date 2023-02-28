3841d91bfb516981a2cb12e5024907beebbe0ce4
Sergei Dorogin <segy@google.com>
2023-02-28 17:59:11 +0400

    test

--- cloud-run-hooks/prebuild.sh
+++ cloud-run-hooks/prebuild.sh
@@ -6,8 +6,25 @@ RED='\033[0;31m' # Red Color
 CYAN='\033[0;36m' # Cyan
 NC='\033[0m' # No Color
 
-echo -n "${CYAN}Would you like to use a google-ads.yaml (Y) - if so, please upload one then enter 'Y', otherewise (N) you'll be asked to enter credentials later:${NC} "
+echo -n -e "${CYAN}Would you like to use a google-ads.yaml (Y) - if so, please upload one then enter 'Y', otherewise (N) you'll be asked to enter credentials later:${NC} "
 read -r USE_GOOGLE_ADS_CONFIG
+while [[ $USE_GOOGLE_ADS_CONFIG = "Y" || $USE_GOOGLE_ADS_CONFIG = "y" ]]; do 
+  # NOTE: the script is executed inside $APP_DIR folder (not where it's located) 
+  if [[ ! -f ./google-ads.yaml && -f ./../google-ads.yaml ]]; then
+    cp ./../google-ads.yaml ./google-ads.yaml
+  fi
+  if [[ ! -f ./google-ads.yaml ]]; then    
+    echo -e "${RED}Could not found google-ads.yaml config file${NC}"
+    echo -n -e "${NC}Please upload google-ads.yaml and enter 'Y' or press Enter to skip${NC}"
+    echo -r USE_GOOGLE_ADS_CONFIG
+  fi
+done
+if [[ $USE_GOOGLE_ADS_CONFIG = "Y" || $USE_GOOGLE_ADS_CONFIG = "y" ]]; then
+  # update Dockerfile to copy google-ads.yaml:
+  sed -i -e "s|##*[[:space:]]*COPY google-ads.yaml \..*$|COPY google-ads.yaml \.|" ./Dockerfile
+  # update answers.json to use google-ads.yaml:
+  sed -i -e "s|"use_googleads_config":[[:space:]]*false,|"use_googleads_config": true,|" ./answers.json
+fi
 
 #echo "K_SERVICE: $K_SERVICE"
 #echo "GOOGLE_CLOUD_REGION: $GOOGLE_CLOUD_REGION"
@@ -29,17 +46,3 @@ gcloud services enable bigquery.googleapis.com
 gcloud services enable cloudfunctions.googleapis.com
 gcloud services enable googleads.googleapis.com
 
-if [[ $USE_GOOGLE_ADS_CONFIG = "Y" || $USE_GOOGLE_ADS_CONFIG = "y" ]]; then
-  echo "using google-ads.yaml"
-  # NOTE: the script is executed inside $APP_DIR folder (not where it's located) 
-  if [[ ! -f ./google-ads.yaml && -f ./../google-ads.yaml ]]; then
-    cp ./../google-ads.yaml ./google-ads.yaml
-  fi
-  if [[ ! -f ./google-ads.yaml ]]; then
-    echo -e "${RED}Could not found google-ads.yaml config file${NC}"
-  fi
-
-  sed -i -e "s|##*[[:space:]]*COPY google-ads.yaml \..*$|COPY google-ads.yaml \.|" ./Dockerfile
-  # TODO: update 
-  # "use_googleads_config": true,
-fi
