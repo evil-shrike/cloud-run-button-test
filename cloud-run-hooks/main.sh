e27fdc1a50417b989a82385298f79eca3a50f01c
Sergei Dorogin <segy@google.com>
2023-02-28 19:41:04 +0400

    test

--- cloud-run-hooks/main.sh
+++ cloud-run-hooks/main.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 set -e
+RED='\033[0;31m' # Red Color
+CYAN='\033[0;36m' # Cyan
+NC='\033[0m' # No Color
 
 if [[ -f ./dashboard_url.txt ]]; then
   # found dashboard_url from previous run
-  echo -e "It seems you have deployed the solution already"
-  echo "If you haven't already, use this url for dashboard cloding:"
+  echo -e "It seems you have deployed the solution already."
+  echo -e "If you haven't already, use this url for dashboard cloning:${CYAN}"
   cat ./dashboard_url.txt
-  echo -e "Would you like to delete the current Cloud Run service (it's needed only for installation)"
+  echo -e "${NC}"
+  echo
+  echo -e "${RED}Would you like to delete the current Cloud Run service (it's needed only for installation) (Y\/N): ${NC}"
   read -r COMMIT_SUICIDE
-  if [[ -n $COMMIT_SUICIDE ]]; then
-    # TODO: kill the current service
-    echo 'TODO: deleting '
-    echo "K_SERVICE: $K_SERVICE"
-    echo "GOOGLE_CLOUD_REGION: $GOOGLE_CLOUD_REGION"
-    echo "GOOGLE_CLOUD_PROJECT: $GOOGLE_CLOUD_PROJECT"
-
-    bash
-    #gcloud run services delete $CR_NAME --region=$CR_REGION
+  if [[ $COMMIT_SUICIDE = 'Y' || $COMMIT_SUICIDE = 'y' ]]; then
+    REGION=$(curl -H Metadata-Flavor:Google http://metadata.google.internal/computeMetadata/v1/instance/region -s --fail)
+    echo "Detected region $REGION"
+    gcloud run services delete $K_SERVICE --region $REGION
   else
     echo "Would you like to restart installation? (Y\/N)"
     read -r REPEAR_INSTALLATION
@@ -29,4 +29,10 @@ if [[ -f ./dashboard_url.txt ]]; then
   fi
 else
   npm init gaarf-wf -- --answers=answers.json
+  # copy dashboard_url.txt to GCS
+  if [[ -f ./dashboard_url.txt ]]; then
+    PROJECT_ID=$(curl -H Metadata-Flavor:Google http://metadata.google.internal/computeMetadata/v1/project/project-id -s --fail)
+    # TODO: hard-code of "gaarftest" :(
+    gsutil -m cp -R ./dashboard_url.txt gs://$PROJECT_ID/gaarftest
+  fi
 fi
