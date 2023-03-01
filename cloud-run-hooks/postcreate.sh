3afe7406ce098b18ff26e1e24bd16a79521e215b
Sergei Dorogin <segy@google.com>
2023-03-01 13:30:59 +0400

    test

--- cloud-run-hooks/postcreate.sh
+++ cloud-run-hooks/postcreate.sh
@@ -2,9 +2,6 @@
 set -e
 set -x
 
-PROJECT_ID=$(curl -H Metadata-Flavor:Google http://metadata.google.internal/computeMetadata/v1/project/project-id -s --fail)
-gsutil -m cp -R ./Dockerfile gs://$PROJECT_ID/gaarftest/
-
 echo 'Updating Cloud Run service $K_SERVICE'
 # ttyd doesn't work on Gen2 environment, so we're updating it to Gen2, additionally increasing request timeout to the maximum allowed
 gcloud run services update $K_SERVICE --execution-environment=gen2 --timeout=3600 --max-instances=1 --region=$GOOGLE_CLOUD_REGION
