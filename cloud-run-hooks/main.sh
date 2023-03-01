00213faf9991dd90a56011ba7ffe69c63fb0c8ee
Sergei Dorogin <segy@google.com>
2023-03-01 15:56:05 +0400

    test

--- cloud-run-hooks/main.sh
+++ cloud-run-hooks/main.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 set -e
-RED='\033[0;31m' # Red Color
-CYAN='\033[0;36m' # Cyan
+
+install() {
+  npm init gaarf-wf -- --answers=answers.json
+}
+
 NC='\033[0m' # No Color
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
 
 if [[ -f ./dashboard_url.txt ]]; then
   # found dashboard_url from previous run
   echo -e "It seems you have deployed the solution already."
-  echo -e "If you haven't already, use this url for dashboard cloning:${CYAN}"
+  echo -e "If you haven't already, use this url for dashboard cloning:${Cyan}"
   cat ./dashboard_url.txt
   echo -e "${NC}"
   echo
-  echo -n -e "${RED}Would you like to delete the current Cloud Run service (it's needed only for installation) (Y/N): ${NC}"
+  echo -n -e "${Red}Would you like to delete the current Cloud Run service (it's needed only for installation) (Y/N): ${NC}"
   read -r COMMIT_SUICIDE
   if [[ $COMMIT_SUICIDE = 'Y' || $COMMIT_SUICIDE = 'y' ]]; then
     REGION=$(curl -H Metadata-Flavor:Google http://metadata.google.internal/computeMetadata/v1/instance/region -s --fail)
@@ -19,21 +30,16 @@ if [[ -f ./dashboard_url.txt ]]; then
     echo "Detected region $REGION"
     gcloud run services delete $K_SERVICE --region $REGION
   else
-    echo "Would you like to restart installation? (Y\/N)"
+    echo -n "Would you like to restart installation? (Y/N): "
     read -r REPEAR_INSTALLATION
-    if [[ -n $REPEAR_INSTALLATION ]]; then
-      npm init gaarf-wf -- --answers=answers.json
+    if [[ $REPEAR_INSTALLATION =~ ^[Yy]$ ]]; then
+      install
     else
       echo "This is the end"
+      bash
       exit
     fi
   fi
 else
-  npm init gaarf-wf -- --answers=answers.json
-  # copy dashboard_url.txt to GCS
-  if [[ -f ./dashboard_url.txt ]]; then
-    PROJECT_ID=$(curl -H Metadata-Flavor:Google http://metadata.google.internal/computeMetadata/v1/project/project-id -s --fail)
-    # TODO: hard-code of "gaarftest" :(
-    gsutil -m cp -R ./dashboard_url.txt gs://$PROJECT_ID/gaarftest/
-  fi
+  install
 fi
